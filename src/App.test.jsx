bdf4bd25dc9d1118b99adfaa9c769a6755ed2b07
Fernando Dodino <fernando.dodino@gmail.com>
2024-02-06 23:14:18 -0300

    Mocking service to have a smoke test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { beforeEach, vi } from 'vitest'
+import { zonaService } from './services/zonaService'
 
-test('smoke test for App', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Consulta/i)
-  expect(linkElement).toBeInTheDocument()
+describe('App', () => {
+
+  beforeEach(() => {
+    vi.spyOn(zonaService, 'zonas').mockImplementation(() => [])
+  })
+
+  test('smoke test for App', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/Consulta/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+  
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
 })
